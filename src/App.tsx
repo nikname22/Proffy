6acf0bcb66f75f99b50599ef4572db095a42cca8
nikolas <ncchemale@idejr.com.br>
2020-08-06 14:43:42 -0300

    Teacher list

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './assets/styles/global.css';
-import Landing from './pages/Landing';
+import Routes from './pages/routes';
 
 
 function App() {
   return (
-    <Landing />
+    <Routes />
   );
 }
 
